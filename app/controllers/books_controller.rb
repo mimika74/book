8e28e771a9440ad54fd50366e296e3b05c3b5453
mimika74 <thfoeichir74mcs@gmail.com>
2021-10-02 11:50:50 +0000

    first commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,59 @@
 class BooksController < ApplicationController
-  def index
-  end
+
 
   def new
+    @book = Book.new
   end
 
-  def show
+  def create
+    @book = Book.new(books_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path
+    else
+    render :index
+    end
   end
 
-  def edit
+   def index
+    @books = Book.all
+    @book = Book.new
+
+   end
+
+
+
+   def show
+    @book = Book.find(params[:id])
+
+   end
+
+   def edit
+    @book = Book.find(params[:id])
+
+
+   end
+
+  def update
+    @book = Book.find(params[:id])
+    book.update(books_params)
+
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book)
+
   end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+  end
+
+end
+
+private
+
+def books_params
+  params.require(:book).permit(:title, :body)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_26_130839) do
+ActiveRecord::Schema.define(version: 2021_09_26_134529) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
